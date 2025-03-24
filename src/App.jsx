2762624e1217f465e2f6a32cee905fc2eae8ded8
Arela-Cristina <instagrandine@gmail.com>
2025-03-25 00:06:04 +0100

    Add form and add style

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,48 @@ function App() {
 
 
   return (
-    <form action="submit">
-      <div>
-        <label htmlFor="name">Scrivi il tuo Nome</label>
-        <input type="text" placeholder='il tuo nome' id="name" name="name" />
-      </div>
+    <>
+      <h2>Il Form degli Sviluppatori</h2>
 
-      <div>
-        <label htmlFor="username">Scrivi il tuo User-Name</label>
-        <input type="text" placeholder='il tuo User-Name' id="username" name="username" />
-      </div>
+      <form action="submit">
+        <div className='customInput'>
+          <label htmlFor="name">Scrivi il tuo Nome</label>
+          <input type="text" placeholder='il tuo nome' id="name" name="name" />
+        </div>
 
-      <div>
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" />
-      </div>
+        <div className='customInput'>
+          <label htmlFor="username">Scrivi il tuo User-Name</label>
+          <input type="text" placeholder='il tuo User-Name' id="username" name="username" />
+        </div>
 
-      <div>
-        <label htmlFor="specialization">Specializzazione</label>
-        <select name="" id="specialization">
-          <option value="frontend">Frontend</option>
-          <option value="backend">Backend</option>
-        </select>
-      </div>
+        <div className='customInput'>
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" />
+        </div>
 
-      <div>
-        <label for="yearsOfExperience">Anni di esperienza</label>
-        <input type='number' id="yearsOfExperience" name="yearsOfExperience" />
-      </div>
+        <div className='customInput'>
+          <label htmlFor="specialization">Specializzazione</label>
+          <select name="" id="specialization">
+            <option value="frontend">Frontend</option>
+            <option value="backend">Backend</option>
+          </select>
+        </div>
 
-      <div>
-        <label for="description">Breve descrizione sullo sviluppatore</label>
-        <textarea id="description" name="description" rows="4" cols="50"></textarea>
-      </div>
+        <div className='customInput'>
+          <label for="yearsOfExperience">Anni di esperienza</label>
+          <input type='number' id="yearsOfExperience" name="yearsOfExperience" />
+        </div>
 
-      <button type='submit'>Inviare</button>
-    </form>
+        <div className='customInput'>
+          <label for="description">Breve descrizione sullo sviluppatore</label>
+          <textarea id="description" name="description" rows="4" cols="50"></textarea>
+        </div>
+
+        <button type='submit'>Inviare</button>
+      </form>
+
+    </>
+
   )
 }
 
