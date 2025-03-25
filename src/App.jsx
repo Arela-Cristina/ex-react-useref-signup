3a1be0e423ce122c4e1b83f89165c12e3c3f0efe
Arela-Cristina <instagrandine@gmail.com>
2025-03-25 15:41:06 +0100

    Millestone 1 work done

--- src/App.jsx
+++ src/App.jsx
@@ -3,58 +3,170 @@ import { useState } from 'react'
 
 function App() {
 
-  const [realName, setRealName] = useState('');
+  console.log('render')
 
-  const [userName, setUserName] = useState('');
 
-  const [password, setPassword] = useState('');
 
-  const [specialization, setSpecialization] = useState('');
+  // stato per controllare se il campo e stato compilato
+  const [isFilled, setIsFilled] = useState(true);
 
-  const [yearsOfExperience, setyearsOfExperience] = useState('');
+  const [submited, setSubmited] = useState(false)
 
-  const [textArea, settextArea] = useState('')
+  const [formData, setFormData] = useState({
+    realName: '',
+    userName: '',
+    password: '',
+    specialization: '',
+    yearsOfExperience: '',
+    textArea: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+    
+    if (name === "specialization" && value !== "") {
+      setIsFilled(true);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const { realName, userName, password, specialization, yearsOfExperience, textArea } = formData;
+
+    if (realName.trim() != '' &&
+      userName.trim() != '' &&
+      password.trim() != '' &&
+      specialization != '' &&
+      Number.isInteger(Number(yearsOfExperience)) &&
+      Number(yearsOfExperience) >= 0 &&
+      textArea.trim() != '') {
+
+      setSubmited(true)
+      console.log('form inviato',
+        {
+          realName,
+          userName,
+          password,
+          specialization,
+          yearsOfExperience,
+          textArea
+        }
+
+      )
+
+
+    } else {
+
+      setSubmited(false)
+      setIsFilled(false)
+      console.log('Ci sono dei campi senza compilare');
+
+    }
+
+  }
+
+  const { realName, userName, password, specialization, yearsOfExperience, textArea } = formData;
 
   return (
     <>
-      <h2>Il Form degli Sviluppatori</h2>
+      <h2>Web Developer Sign Up!</h2>
 
-      <form action="submit">
+      <form onSubmit={handleSubmit}>
         <div className='customInput'>
-          <label htmlFor="name">Scrivi il tuo Nome</label>
-          <input type="text" placeholder='il tuo nome' id="name" name="name" />
+          <label htmlFor="realName">Scrivi il tuo Nome</label>
+          <input
+            type="text"
+            placeholder='il tuo nome'
+            id="realName"
+            name="realName"
+            value={realName}
+            onChange={handleChange}
+            required
+          />
         </div>
 
         <div className='customInput'>
-          <label htmlFor="username">Scrivi il tuo User-Name</label>
-          <input type="text" placeholder='il tuo User-Name' id="username" name="username" />
+          <label htmlFor="userName">Scrivi il tuo User-Name</label>
+          <input
+            type="text"
+            placeholder='il tuo User-Name'
+            id="userName"
+            name="userName"
+            value={userName}
+            onChange={handleChange}
+            required
+          />
+
         </div>
 
         <div className='customInput'>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" />
-        </div>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            required
+          />
+
+        </div >
 
         <div className='customInput'>
           <label htmlFor="specialization">Specializzazione</label>
-          <select name="" id="specialization">
+          <select
+            name="specialization"
+            id="specialization"
+            value={specialization}
+            onChange={handleChange}>
+
+            <option value="">Scegli una specializazzione</option>
             <option value="frontend">Frontend</option>
             <option value="backend">Backend</option>
           </select>
-        </div>
+
+        </div >
 
         <div className='customInput'>
           <label htmlFor="yearsOfExperience">Anni di esperienza</label>
-          <input type='number' id="yearsOfExperience" name="yearsOfExperience" />
+          <input
+            type='number'
+            id="yearsOfExperience"
+            name="yearsOfExperience"
+            min="0"
+            value={yearsOfExperience}
+            onChange={handleChange}
+            required
+          />
+
         </div>
 
         <div className='customInput'>
-          <label htmlFor="description">Breve descrizione sullo sviluppatore</label>
-          <textarea id="description" name="description" rows="4" cols="50"></textarea>
+          <label htmlFor="textArea">Breve descrizione sullo sviluppatore</label>
+          <textarea
+            id="textArea"
+            name="textArea"
+            placeholder='Io mi chiamo ... '
+            rows="4"
+            cols="50"
+            value={textArea}
+            onChange={handleChange}
+            required
+          ></textarea>
+
         </div>
 
         <button type='submit'>Inviare</button>
-      </form>
+
+      </form >
+      {submited && <p> Form Inviato!</p >}
+      {!isFilled && <p className='errorText'> Scegli una Specializazzione!</p >}
 
     </>
 
